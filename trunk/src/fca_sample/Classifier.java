20df6da704342b9f4b35c12a461158bf581bb4e6
ivandietz <ivandietz@8855b0a9-cef7-0f55-a3ea-11b79ad67954>
2010-03-20 19:50:02 +0000

    Project startup

---
+++ trunk/src/fca_sample/Classifier.java
@@ -0,0 +1,152 @@
+package fca_sample;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.swt.widgets.TableItem;
+
+public class Classifier {
+
+  public static Criteria classify(TableItem item, HashMap classHierarchy) {
+    if (classNameInKeyboard(item))
+      return Criteria.CLASS_NAME_IN_KEYBOARD;
+    if (classNameInParameter(item))
+      return Criteria.CLASS_NAME_IN_PARAMETER;
+    if (classesOnly(item))
+      return Criteria.CLASSES_ONLY;
+    if (hierarchyMethod(item, classHierarchy))
+      return Criteria.HIERARCHY_METHOD;
+    if (crosscuttingMethod(item, classHierarchy))
+      return Criteria.CROSSCUTTING_METHOD;
+    return Criteria.NONE;
+  }
+
+  private static boolean classNameInKeyboard(TableItem item) {
+    boolean hasMethod = false;
+    String elements[] = item.getText(1).split(", ");
+    for (int i = 0; i < elements.length; i++) {
+      String components[] = elements[i].split(":");
+      if (components.length <= 2) {
+        if (components.length == 2) {
+          hasMethod = true;
+          if (!components[1].contains(components[0]))
+            return false;
+        }
+      } else
+        return false;
+    }
+    return hasMethod;
+  }
+
+  private static boolean classNameInParameter(TableItem item) {
+    boolean hasParameter = false;
+    String elements[] = item.getText(1).split(", ");
+    for (int i = 0; i < elements.length; i++) {
+      String components[] = elements[i].split(":");
+      if (components.length == 1 || components.length == 3) {
+        if (components.length == 3) {
+          hasParameter = true;
+          if (!components[2].contains(components[0]))
+            return false;
+        }
+      } else
+        return false;
+    }
+    return hasParameter;
+  }
+
+  private static boolean classesOnly(TableItem item) {
+    String elements[] = item.getText(1).split(", ");
+    for (int i = 0; i < elements.length; i++) {
+      String components[] = elements[i].split(":");
+      if (!(components.length == 1))
+        return false;
+    }
+    return true;
+  }
+
+  private static boolean hierarchyMethod(TableItem item, HashMap hierarchy) {
+    HashMap methodHierarchy = new HashMap<String, List<String>>();
+    Set<String> classNames = new HashSet<String>();
+    String elements[] = item.getText(1).split(", ");
+    for (int i = 0; i < elements.length; i++) {
+      String components[] = elements[i].split(":");
+      if (components.length == 2) {
+        if (!classNames.contains(components[0])) {
+          classNames.add(components[0]);
+          methodHierarchy.put(elements[i], getHierarchy(components[0], hierarchy));
+        } else
+          return false; // methods must be from different classes
+      } else
+        return false; // we only want methods
+    }
+    // search for shared parents
+    Iterator<String> i = methodHierarchy.keySet().iterator();
+    String element = i.next();
+    List<String> elementParents = (List<String>) methodHierarchy.get(element);
+    boolean shareParents = true;
+    while (i.hasNext() && shareParents) {
+      shareParents = false;
+      String nextElement = i.next();
+      List<String> parents = (List<String>) methodHierarchy.get(nextElement);
+      for (Iterator iterator = parents.iterator(); iterator.hasNext() && !shareParents;) {
+        String superclass = (String) iterator.next();
+        if (elementParents.contains(superclass)) {
+          shareParents = true;
+        }
+      }
+    }
+    return shareParents;
+  }
+
+  private static boolean crosscuttingMethod(TableItem item, HashMap hierarchy) {
+    HashMap methodHierarchy = new HashMap<String, List<String>>();
+    Set<String> classNames = new HashSet<String>();
+    String elements[] = item.getText(1).split(", ");
+    for (int i = 0; i < elements.length; i++) {
+      String components[] = elements[i].split(":");
+      if (components.length == 2) {
+        if (!classNames.contains(components[0])) {
+          classNames.add(components[0]);
+          methodHierarchy.put(elements[i], getHierarchy(components[0], hierarchy));
+        } else
+          return false; // methods must be from different classes
+      } else
+        return false; // we only want methods
+    }
+    // search for shared parents
+    Iterator<String> i = methodHierarchy.keySet().iterator();
+    String element = i.next();
+    List<String> elementParents = (List<String>) methodHierarchy.get(element);
+    boolean notShareParents = true;
+    while (i.hasNext() && notShareParents) {
+      notShareParents = false;
+      String nextElement = i.next();
+      List<String> parents = (List<String>) methodHierarchy.get(nextElement);
+      for (Iterator iterator = parents.iterator(); iterator.hasNext() && !notShareParents;) {
+        String superclass = (String) iterator.next();
+        if (!elementParents.contains(superclass)) {
+          notShareParents = true;
+        }
+      }
+    }
+    return notShareParents;
+  }
+  
+  
+  private static List<String> getHierarchy(String className, HashMap hierarchyMap) {
+    List<String> list = new ArrayList<String>();
+    list.add(className);
+    String father = (String) hierarchyMap.get(className);
+    while (father != null) {
+      list.add(father);
+      father = (String) hierarchyMap.get(father);
+    }
+
+    return list;
+  }
+}
---
+++ trunk/src/fca_sample/FCAImplementation.java
@@ -0,0 +1,108 @@
+package fca_sample;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jface.text.Document;
+
+import colibri.lib.Concept;
+import colibri.lib.HybridLattice;
+import colibri.lib.Lattice;
+import colibri.lib.Relation;
+import colibri.lib.Traversal;
+import colibri.lib.TreeRelation;
+
+public class FCAImplementation {
+
+  private Lattice lattice = null;
+  private HashMap<String, String> hierarchy = null;  
+
+  public FCAImplementation(IProject project, boolean useClasses,
+      boolean useMethods, boolean useParams) throws CoreException {
+    hierarchy = new HashMap<String,String>();
+    if (project.isNatureEnabled("org.eclipse.jdt.core.javanature")) {
+      IJavaProject javaProject = JavaCore.create(project);
+      createLattice(javaProject, useClasses, useMethods, useParams);
+    }
+  }
+
+  private void createLattice(IJavaProject javaProject, boolean useClasses,
+      boolean useMethods, boolean useParams) throws JavaModelException {
+    Relation rel = new TreeRelation();
+    Pattern p = Pattern.compile("[a-zA-Z]{1}[a-z]*");
+    Matcher matcher;
+
+    IPackageFragment[] packages = javaProject.getPackageFragments();
+    for (IPackageFragment mypackage : packages) {
+      // Package fragments include all packages in the classpath
+      // We will only look at the package from the source folder
+      // K_BINARY would include also included JARS, e.g. rt.jar
+      if (mypackage.getKind() == IPackageFragmentRoot.K_SOURCE) {
+        for (ICompilationUnit unit : mypackage.getCompilationUnits()) {
+          Document doc = new Document(unit.getSource());
+          IType[] allTypes = unit.getAllTypes();
+          for (IType type : allTypes) {
+            hierarchy.put(type.getElementName(), type.getSuperclassName());
+            matcher = p.matcher(type.getElementName());
+            while (matcher.find()) {
+              if (useClasses && !Filter.isStopWord(matcher.group().toLowerCase()))
+                rel.add(type.getElementName(), matcher.group().toLowerCase());
+            }
+            IMethod[] methods = type.getMethods();
+            for (IMethod method : methods) {
+              matcher = p.matcher(method.getElementName());
+              while (matcher.find()) {
+                if (useMethods && !Filter.isStopWord(matcher.group().toLowerCase()))
+                  if (!type.getElementName().equals(method.getElementName()))
+                    rel.add(type.getElementName() + ":" + method.getElementName(), matcher.group().toLowerCase());
+              }
+              String[] parameterNames = method.getParameterNames();
+              for (int i = 0; i < parameterNames.length; i++) {
+                matcher = p.matcher(parameterNames[i]);
+                while (matcher.find()) {
+                  if (useParams && !Filter.isStopWord(matcher.group().toLowerCase()))
+                    rel.add(type.getElementName() + ":" + method.getElementName() + ":" + parameterNames[i], matcher.group().toLowerCase());
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    lattice = new HybridLattice(rel);
+  }
+
+  public List<Concept> getConcepts() {
+    List<Concept> concepts = new ArrayList<Concept>();
+    if (lattice != null) {
+      // get the iterator
+      Iterator<Concept> it = lattice.conceptIterator(Traversal.TOP_OBJSIZE);
+
+      while (it.hasNext()) {
+        Concept c = it.next();
+        // filter by size
+        if (c.getAttributes().size() > 0 && c.getObjects().size() > 1)
+          concepts.add(c);
+      }
+    }
+    return concepts;
+  }
+  
+  public HashMap getHierarchy(){
+    return hierarchy;
+  }
+}
