69bddd936d0642a685f1132c877357f4081b97a3
cristianvitale12 <cristianvitale12@8855b0a9-cef7-0f55-a3ea-11b79ad67954>
2010-05-15 17:48:45 +0000

    Algunas mejoras. Agregado del tree y tamaño de ventana fijo.

--- trunk/src/fca_sample/actions/Window.java
+++ trunk/src/fca_sample/actions/Window.java
@@ -11,6 +11,11 @@ import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -28,8 +33,13 @@ import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
 
 import colibri.lib.Concept;
+
+import com.swtdesigner.SWTResourceManager;
+
 import fca_sample.ClassifiedTableItem;
 import fca_sample.Classifier;
 import fca_sample.Criteria;
@@ -39,9 +49,11 @@ public class Window {
 
   protected Shell shlFcaSample;
   private Combo projectsCombo;
+  private Tree tree;
   private Button btnClasses;
   private Button btnMethods;
   private Button btnParameters;
+  private Label lblRunning;
   private Map<String, IProject> projectsMap;
   private Table resultsTable;
   private Table detailsTable;
@@ -87,12 +99,12 @@ public class Window {
    * Create contents of the window.
    */
   protected void createContents() {
-    shlFcaSample = new Shell();
-    shlFcaSample.setSize(752, 575);
+    shlFcaSample = new Shell(SWT.MIN);
+    shlFcaSample.setSize(1000, 580);
     shlFcaSample.setText("FCA Sample");
     {
       TabFolder tabFolder = new TabFolder(shlFcaSample, SWT.NONE);
-      tabFolder.setBounds(0, 10, 739, 522);
+      tabFolder.setBounds(0, 10, 992, 522);
       {
         TabItem tbtmSelectProjects = new TabItem(tabFolder, SWT.NONE);
         tbtmSelectProjects.setText("Select Projects");
@@ -108,6 +120,16 @@ public class Window {
               Map.Entry project = (Map.Entry) it.next();
               projectsCombo.add(project.getKey().toString());
             }
+            projectsCombo.addSelectionListener(new SelectionListener() {
+              @Override
+              public void widgetDefaultSelected(SelectionEvent e) {
+              }
+
+              @Override
+              public void widgetSelected(SelectionEvent e) {
+                fillTree(projectsMap.get(projectsCombo.getText()));
+              }
+            });
           }
           {
             Button btnRunFcaAlgorithm = new Button(composite, SWT.NONE);
@@ -116,16 +138,17 @@ public class Window {
               public void widgetSelected(SelectionEvent e) {
                 if (!projectsCombo.getText().equals("")) {
                   try {
+                    lblRunning.setText("Running...");
                     fca = new FCAImplementation(projectsMap.get(projectsCombo.getText()), btnClasses.getSelection(), btnMethods.getSelection(), btnParameters.getSelection());
                     showResults(fca.getConcepts());
-                    
+                    lblRunning.setText("");
                   } catch (CoreException e1) {
                     e1.printStackTrace();
                   }
                 }
               }
             });
-            btnRunFcaAlgorithm.setBounds(10, 154, 146, 23);
+            btnRunFcaAlgorithm.setBounds(10, 159, 146, 23);
             btnRunFcaAlgorithm.setText("Run FCA Algorithm");
           }
           {
@@ -156,6 +179,15 @@ public class Window {
             lblSelectProject.setBounds(10, 23, 66, 13);
             lblSelectProject.setText("Select Project");
           }
+          {
+            lblRunning = new Label(composite, SWT.NONE);
+            lblRunning.setFont(SWTResourceManager.getFont("Tahoma", 8, SWT.BOLD));
+            lblRunning.setBounds(193, 164, 66, 13);
+          }
+          {
+            tree = new Tree(composite, SWT.BORDER | SWT.CHECK | SWT.V_SCROLL | SWT.H_SCROLL);
+            tree.setBounds(332, 42, 300, 444);
+          }
         }
       }
       {
@@ -183,20 +215,20 @@ public class Window {
           {
             Group grpLatticeConcepts = new Group(composite, SWT.NONE);
             grpLatticeConcepts.setText("Lattice Concepts");
-            grpLatticeConcepts.setBounds(10, 10, 345, 447);
+            grpLatticeConcepts.setBounds(10, 10, 460, 447);
             {
               resultsTable = new Table(grpLatticeConcepts, SWT.BORDER | SWT.CHECK | SWT.FULL_SELECTION);
-              resultsTable.setBounds(10, 21, 325, 416);
+              resultsTable.setBounds(10, 21, 440, 416);
               resultsTable.setLinesVisible(true);
               resultsTable.setHeaderVisible(true);
               {
                 TableColumn tblclmnAttributes_1 = new TableColumn(resultsTable, SWT.NONE);
-                tblclmnAttributes_1.setWidth(152);
+                tblclmnAttributes_1.setWidth(182);
                 tblclmnAttributes_1.setText("Attributes");
               }
               {
                 TableColumn tblclmnElements_1 = new TableColumn(resultsTable, SWT.NONE);
-                tblclmnElements_1.setWidth(100);
+                tblclmnElements_1.setWidth(242);
                 tblclmnElements_1.setText("Elements");
               }
               resultsTable.addSelectionListener(new SelectionListener() {
@@ -217,25 +249,25 @@ public class Window {
           {
             Group grpConceptDetails = new Group(composite, SWT.NONE);
             grpConceptDetails.setText("Concept Details");
-            grpConceptDetails.setBounds(361, 10, 360, 447);
+            grpConceptDetails.setBounds(476, 10, 498, 447);
             {
               detailsTable = new Table(grpConceptDetails, SWT.BORDER | SWT.FULL_SELECTION);
-              detailsTable.setBounds(10, 46, 340, 391);
+              detailsTable.setBounds(10, 46, 478, 391);
               detailsTable.setHeaderVisible(true);
               detailsTable.setLinesVisible(true);
               {
                 TableColumn tblclmnClassName = new TableColumn(detailsTable, SWT.NONE);
-                tblclmnClassName.setWidth(100);
+                tblclmnClassName.setWidth(134);
                 tblclmnClassName.setText("Class Name");
               }
               {
                 TableColumn tblclmnMethodName = new TableColumn(detailsTable, SWT.NONE);
-                tblclmnMethodName.setWidth(100);
+                tblclmnMethodName.setWidth(143);
                 tblclmnMethodName.setText("Method Name");
               }
               {
                 TableColumn tblclmnParameterName = new TableColumn(detailsTable, SWT.NONE);
-                tblclmnParameterName.setWidth(100);
+                tblclmnParameterName.setWidth(125);
                 tblclmnParameterName.setText("Parameter Name");
               }
             }
@@ -243,7 +275,7 @@ public class Window {
               attributesText = new Text(grpConceptDetails, SWT.BORDER);
               attributesText.setEditable(false);
               attributesText.setEnabled(false);
-              attributesText.setBounds(65, 21, 294, 19);
+              attributesText.setBounds(65, 21, 423, 19);
             }
             {
               Label lblAttributes = new Label(grpConceptDetails, SWT.NONE);
@@ -261,12 +293,12 @@ public class Window {
           tbtmClassification.setControl(composite);
           {
             Group grpConcepts = new Group(composite, SWT.NONE);
-            grpConcepts.setBounds(10, 37, 346, 420);
+            grpConcepts.setBounds(10, 37, 460, 420);
             grpConcepts.setText("Concepts");
             {
               criteriaCombo = new Combo(composite, SWT.READ_ONLY);
               criteriaCombo.setVisibleItemCount(10);
-              criteriaCombo.setBounds(65, 10, 291, 21);
+              criteriaCombo.setBounds(65, 10, 266, 21);
               Criteria[] criterias = Criteria.values();
               for (int i = 0; i < criterias.length; i++) {
                 criteriaCombo.add(criterias[i].getName());
@@ -287,15 +319,15 @@ public class Window {
               classifiedConceptsTable = new Table(grpConcepts, SWT.BORDER | SWT.FULL_SELECTION);
               classifiedConceptsTable.setLinesVisible(true);
               classifiedConceptsTable.setHeaderVisible(true);
-              classifiedConceptsTable.setBounds(10, 21, 326, 389);
+              classifiedConceptsTable.setBounds(10, 21, 440, 389);
               {
                 TableColumn tblclmnAttributes = new TableColumn(classifiedConceptsTable, SWT.NONE);
-                tblclmnAttributes.setWidth(152);
+                tblclmnAttributes.setWidth(155);
                 tblclmnAttributes.setText("Attributes");
               }
               {
                 TableColumn tblclmnElements = new TableColumn(classifiedConceptsTable, SWT.NONE);
-                tblclmnElements.setWidth(100);
+                tblclmnElements.setWidth(191);
                 tblclmnElements.setText("Elements");
               }
               classifiedConceptsTable.addSelectionListener(new SelectionListener() {
@@ -315,26 +347,26 @@ public class Window {
           }
           {
             Group group = new Group(composite, SWT.NONE);
-            group.setBounds(362, 37, 359, 420);
+            group.setBounds(476, 37, 498, 420);
             group.setText("Concept Details");
             {
               classifiedDetailsTable = new Table(group, SWT.BORDER | SWT.FULL_SELECTION);
               classifiedDetailsTable.setLinesVisible(true);
               classifiedDetailsTable.setHeaderVisible(true);
-              classifiedDetailsTable.setBounds(10, 46, 339, 364);
+              classifiedDetailsTable.setBounds(10, 46, 478, 364);
               {
                 TableColumn tableColumn = new TableColumn(classifiedDetailsTable, SWT.NONE);
-                tableColumn.setWidth(100);
+                tableColumn.setWidth(147);
                 tableColumn.setText("Class Name");
               }
               {
                 TableColumn tableColumn = new TableColumn(classifiedDetailsTable, SWT.NONE);
-                tableColumn.setWidth(100);
+                tableColumn.setWidth(148);
                 tableColumn.setText("Method Name");
               }
               {
                 TableColumn tableColumn = new TableColumn(classifiedDetailsTable, SWT.NONE);
-                tableColumn.setWidth(100);
+                tableColumn.setWidth(137);
                 tableColumn.setText("Parameter Name");
               }
             }
@@ -342,7 +374,7 @@ public class Window {
               classifiedAttributeText = new Text(group, SWT.BORDER);
               classifiedAttributeText.setEnabled(false);
               classifiedAttributeText.setEditable(false);
-              classifiedAttributeText.setBounds(65, 21, 284, 19);
+              classifiedAttributeText.setBounds(65, 21, 423, 19);
             }
             {
               Label label = new Label(group, SWT.NONE);
@@ -445,4 +477,40 @@ public class Window {
       classifiedConceptsTable.getColumn(i).pack();
     shlFcaSample.update();
   }
+  
+  private void fillTree(IProject project) {
+    tree.removeAll();
+    HashMap<String, TreeItem> hash = new HashMap<String, TreeItem>();
+    try {
+      if (project.isNatureEnabled("org.eclipse.jdt.core.javanature")) {
+        IJavaProject javaProject = JavaCore.create(project);
+        IPackageFragment[] packages = javaProject.getPackageFragments();
+        for (IPackageFragment mypackage : packages) {
+          if (mypackage.getKind() == IPackageFragmentRoot.K_SOURCE) {
+            if (!mypackage.getElementName().contains(".")) {
+              TreeItem itemTree = new TreeItem(tree, SWT.NONE);
+              String packageName;
+              if (mypackage.getElementName().equals(""))
+                packageName = "(default package)";
+              else
+                packageName = mypackage.getElementName();
+              itemTree.setText(packageName);
+              hash.put(mypackage.getElementName(), itemTree);
+            } else {
+              String elementName = mypackage.getElementName();
+              TreeItem parent = hash.get(elementName.substring(0, elementName.lastIndexOf(".")));
+              TreeItem itemTree = new TreeItem(parent, SWT.NONE);
+              itemTree.setText(elementName.substring(elementName.lastIndexOf(".") + 1, elementName.length()));
+              hash.put(elementName, itemTree);
+            }
+          }
+        }
+      }
+    } catch (JavaModelException e) {
+      e.printStackTrace();
+    } catch (CoreException e) {
+      e.printStackTrace();
+    }
+  }
+  
 }
