055d2c083bbbde34ff3c1a84c5dbe2b57184c8f4
ivandietz <ivandietz@8855b0a9-cef7-0f55-a3ea-11b79ad67954>
2010-06-20 21:34:10 +0000

    Se agrego el test para verbos en WordNet

--- src/fca_sample/TestWordNet.java
+++ src/fca_sample/TestWordNet.java
@@ -3,6 +3,7 @@ package fca_sample;
 import edu.smu.tspell.wordnet.NounSynset;
 import edu.smu.tspell.wordnet.Synset;
 import edu.smu.tspell.wordnet.SynsetType;
+import edu.smu.tspell.wordnet.VerbSynset;
 import edu.smu.tspell.wordnet.WordNetDatabase;
 
 public class TestWordNet {
@@ -14,26 +15,53 @@ public class TestWordNet {
     System.setProperty("wordnet.database.dir", "C:\\Archivos de programa\\WordNet\\2.1\\dict");
     NounSynset nounSynset;
     NounSynset[] hyponyms;
+    VerbSynset verbSynset;
+    VerbSynset[] troponyms;
+    
 
+    //VERBS
     WordNetDatabase database = WordNetDatabase.getFileInstance();
-    Synset[] synsets = database.getSynsets("car", SynsetType.NOUN);
+    Synset[] synsets = database.getSynsets("stop", SynsetType.VERB);
     for (int i = 0; i < synsets.length; i++) {
-        nounSynset = (NounSynset)(synsets[i]);
-        System.out.println("------ Definicion: " + nounSynset.getDefinition());
-        String[] wordForms = nounSynset.getWordForms();
+        verbSynset = (VerbSynset)(synsets[i]);
+        System.out.println("------ Definicion: " + verbSynset.getDefinition());
+        String[] wordForms = verbSynset.getWordForms();
         System.out.println("- WordForms:");
         for (int j = 0; j < wordForms.length; j++) {
           System.out.println(wordForms[j]);
         }
-        System.out.println("---- Hiponimos:");
-        hyponyms = nounSynset.getHyponyms();
-        for (int j = 0; j < hyponyms.length; j++) {
-          String[] wordForms2 = hyponyms[j].getWordForms();
+        System.out.println("---- Troponimos:");
+        troponyms = verbSynset.getTroponyms();
+        for (int j = 0; j < troponyms.length; j++) {
+          String[] wordForms2 = troponyms[j].getWordForms();
           System.out.println("- WordForms:");
           for (int k = 0; k < wordForms2.length; k++) {
             System.out.println(wordForms2[k]);
           }
         }
+    
+    
+    
+    //NOUNS
+//    WordNetDatabase database = WordNetDatabase.getFileInstance();
+//    Synset[] synsets = database.getSynsets("car", SynsetType.NOUN);
+//    for (int i = 0; i < synsets.length; i++) {
+//        nounSynset = (NounSynset)(synsets[i]);
+//        System.out.println("------ Definicion: " + nounSynset.getDefinition());
+//        String[] wordForms = nounSynset.getWordForms();
+//        System.out.println("- WordForms:");
+//        for (int j = 0; j < wordForms.length; j++) {
+//          System.out.println(wordForms[j]);
+//        }
+//        System.out.println("---- Hiponimos:");
+//        hyponyms = nounSynset.getHyponyms();
+//        for (int j = 0; j < hyponyms.length; j++) {
+//          String[] wordForms2 = hyponyms[j].getWordForms();
+//          System.out.println("- WordForms:");
+//          for (int k = 0; k < wordForms2.length; k++) {
+//            System.out.println(wordForms2[k]);
+//          }
+//        }
         
         
 //        hyponyms = nounSynset.getHyponyms();
