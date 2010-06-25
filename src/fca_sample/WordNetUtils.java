08ca57090f07864a98ef756fa9ea03efa77d45fc
cristianvitale12 <cristianvitale12@8855b0a9-cef7-0f55-a3ea-11b79ad67954>
2010-06-25 23:42:29 +0000

    Clase WordNetUtils y algunos cambios menores.

---
+++ src/fca_sample/WordNetUtils.java
@@ -0,0 +1,28 @@
+package fca_sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// TODO Implementar todo!
+public final class WordNetUtils {
+
+  public static List<String> getVerbSynonyms() {
+    List<String> synonyms = new ArrayList<String>();
+    return synonyms;
+  }
+  
+  public static List<String> getVerbAntonyms() {
+    List<String> antonyms = new ArrayList<String>();
+    return antonyms;
+  }
+  
+  public static List<String> getNounSynonyms() {
+    List<String> synonyms = new ArrayList<String>();
+    return synonyms;
+  }
+  
+  public static List<String> getNounAntonyms() {
+    List<String> antonyms = new ArrayList<String>();
+    return antonyms;
+  }
+}
